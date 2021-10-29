40aa74867585b300e54c7f06145b1cb9c852ee4c
NestorCorvus <nestor.lima@corvuslab.co>
2021-10-29 12:00:05 -0500

    se realizo el modulo de variables apu

--- main/app/Http/Controllers/MachineToolController.php
+++ main/app/Http/Controllers/MachineToolController.php
@@ -2,9 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ExternalProcess;
+use App\Models\InternalProcess;
 use App\Models\MachineTool;
 use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MachineToolController extends Controller
 {
@@ -48,18 +51,46 @@ class MachineToolController extends Controller
     public function store(Request $request)
     {
         try {
-            $unit = MachineTool::updateOrCreate(['id' => $request->get('id')], $request->all());
-            return ($unit->wasRecentlyCreated)
-            ?
-            $this->success([
-            'title' => '¡Creado con éxito!',
-            'text' => 'La maquina ha sido creado satisfactoriamente'
-            ])
-            :
-            $this->success([
-            'title' => '¡Actualizado con éxito!',
-            'text' => 'La maquina ha sido Actualizado satisfactoriamente'
-            ]);
+            if ($request->type_id == '1') {
+                $unit = MachineTool::updateOrCreate(['id' => $request->get('id')], $request->all());
+                return ($unit->wasRecentlyCreated)
+                ?
+                $this->success([
+                'title' => '¡Creado con éxito!',
+                'text' => 'La maquina ha sido creado satisfactoriamente'
+                ])
+                :
+                $this->success([
+                'title' => '¡Actualizado con éxito!',
+                'text' => 'La maquina ha sido Actualizado satisfactoriamente'
+                ]);
+            } else if ($request->type_id == '2') {
+                $unit = InternalProcess::updateOrCreate(['id' => $request->get('id')], $request->all());
+                return ($unit->wasRecentlyCreated)
+                ?
+                $this->success([
+                'title' => '¡Creado con éxito!',
+                'text' => 'El proceso ha sido creado satisfactoriamente'
+                ])
+                :
+                $this->success([
+                'title' => '¡Actualizado con éxito!',
+                'text' => 'El proceso ha sido Actualizado satisfactoriamente'
+                ]); 
+            } else if ($request->type_id == '3') {
+                $unit = ExternalProcess::updateOrCreate(['id' => $request->get('id')], $request->all());
+                return ($unit->wasRecentlyCreated)
+                ?
+                $this->success([
+                'title' => '¡Creado con éxito!',
+                'text' => 'El proceso ha sido creado satisfactoriamente'
+                ])
+                :
+                $this->success([
+                'title' => '¡Actualizado con éxito!',
+                'text' => 'El proceso ha sido Actualizado satisfactoriamente'
+                ]);
+            }
         } catch (\Throwable $th) {
             return $this->error($th->getMessage(), 500);
         }
@@ -86,14 +117,54 @@ class MachineToolController extends Controller
 
     public function paginate()
     {
-        return $this->success(
+        $machine = DB::table('machine_tools as m')
+        ->select(
+            'm.id',
+            'm.name',
+            'm.unit_cost',
+            'm.created_at',
+            DB::raw('"Maquinas Herramientas" as type'),
+            DB::raw('"1" as type_id'),
+            'u.name as unit_name',
+            'u.id as unit_id'
+        )
+        ->join('units as u', 'u.id', '=', 'm.unit_id');
+        $internalProcesses = DB::table('internal_processes as i')
+        ->select(
+            'i.id',
+            'i.name',
+            'i.unit_cost',
+            'i.created_at',
+            DB::raw('"Procesos Internos" as type'),
+            DB::raw('"2" as type_id'),
+            'u.name as unit_name',
+            'u.id as unit_id'
+            )
+            ->join('units as u', 'u.id', '=', 'i.unit_id');
+        $externalProcesses = DB::table('external_processes as e')
+        ->select(
+            'e.id',
+            'e.name',
+            'e.unit_cost',
+            'e.created_at',
+            DB::raw('"Procesos Externos" as type'),
+            DB::raw('"3" as type_id'),
+            'u.name as unit_name',
+            'u.id as unit_id'
+        )
+        ->join('units as u', 'u.id', '=', 'e.unit_id')
+        ->union($internalProcesses)
+        ->union($machine)
+        ->paginate(request()->get('pageSize', 10), ['*'], 'page', request()->get('page', 1));
+        return $this->success($externalProcesses);
+        /* return $this->success(
             MachineTool::with('unit')
             ->orderBy('name')
                 ->when(request()->get('name'), function ($q, $fill) {
                     $q->where('name', 'like', '%' . $fill . '%');
                 })
                 ->paginate(request()->get('pageSize', 10), ['*'], 'page', request()->get('page', 1))
-        );
+        ); */
     }
 
     /**
