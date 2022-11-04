4ee94e6785e451a9ba497e034bab02cf4226b5e6
CesarFlorezCorvusLab <cesar.florez@corvuslab.co>
2022-11-04 18:02:56 -0500

    MM-54 - En Proceso

--- main/routes/console.php
+++ main/routes/console.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\WorkContract;
+use Carbon\Carbon;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
@@ -17,3 +19,16 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('listarUsers', function () {
+    $contratos = WorkContract::
+    whereNotNull('date_end')
+    ->get(["person_id as persona", "liquidated", "work_contract_type_id", "date_end"]);
+
+    $hoy=Carbon::now();
+    foreach($contratos as $contrato){
+        $fecha_salida = Carbon::parse($contrato->date_end);
+        $contrato['dayDiff'] = $hoy->diffInDays($fecha_salida,false);
+    }
+    echo "{\"prueba\":".$contratos->where('dayDiff','>',30)->where('dayDiff','<=',45)."}";
+})->purpose('Display a list of users');
