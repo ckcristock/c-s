2abcab931cbfda7f4441ee535fce1e378094409f
CesarFlorezCorvusLab <cesar.florez@corvuslab.co>
2022-11-08 18:32:57 -0500

    MM-54 en Proceso

--- main/app/Console/Kernel.php
+++ main/app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command('listarUsers')->everyMinute()->sendOutputTo('prueba.json');
+        $schedule->command('listarUsers')->everyMinute()->sendOutputTo('../../prueba.json');
     }
 
     /**
--- main/routes/console.php
+++ main/routes/console.php
@@ -1,9 +1,11 @@
 <?php
 
+use App\Models\Alert;
 use App\Models\WorkContract;
 use Carbon\Carbon;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,14 +23,50 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Artisan::command('listarUsers', function () {
-    $contratos = WorkContract::
-    whereNotNull('date_end')
-    ->get(["person_id as persona", "liquidated", "work_contract_type_id", "date_end"]);
+    /* $contratos = WorkContract::
+    whereNotNull('date_end')->orderBy('date_end', 'Desc')
+    ->join('alerts', 'work_contracts.person_id', '!=', 'alerts.user_id')
+    ->get(["work_contracts.person_id as persona", "liquidated", "work_contract_type_id", "date_end",
+        DB::raw("date_format(alerts.created_at,'%Y-%m-%d') as fecha_notif"),
+        DB::raw("datediff(date_end,curdate()) as dayDiff, datediff(date_end,alerts.created_at) as alertDiff")
+    ]); */
 
-    $hoy=Carbon::now();
+    $contratosNotificados = WorkContract::join('alerts', 'work_contracts.person_id', '=', 'alerts.user_id')
+    ->get(["w.person_id as persona", "date_end",
+	DB::raw("datediff(date_end,curdate()) as dayDiff, date_format(alerts.created_at,'%Y-%m-%d') as fecha_notif,
+	 CONCAT('Se renovará el próximo ',date_end) as Estado")
+    ]);
+
+    $contratosYNotificaciones = WorkContract::
+    whereNotIn("persona",Alert::get("user_id"))->union($contratosNotificados)
+    ->get(["w.person_id as persona", "date_end",
+	DB::raw("datediff(date_end,curdate()) as dayDiff, NULL as fecha_notif,
+    case
+        when datediff(date_end,CURDATE()) BETWEEN 30 AND 45 then concat('Debe notificarse antes del ', adddate(date_end, -30))
+        when datediff(date_end,CURDATE()) < 30 then CONCAT('Se renovará automáticamente el próximo ',date_end)
+        ELSE 'Aún no requiere notificación'
+    end as Estado")
+    ]);
+
+    $contratos = DB::table($contratosYNotificaciones)->whereNotNull('date_end')->orderBy('date_end', 'Desc')
+    ->whereRaw("datediff(date_end,curdate()) > 0")
+    ->get();
+
+
+
+    //$hoy=Carbon::now();
     foreach($contratos as $contrato){
-        $fecha_salida = Carbon::parse($contrato->date_end);
-        $contrato['dayDiff'] = $hoy->diffInDays($fecha_salida,false);
+        //$fecha_notificacion = Carbon::parse($contrato->fecha_notif);
+        //$fecha_salida = Carbon::parse($contrato->date_end);
+        //$contrato['dayDiff'] = $hoy->diffInDays($fecha_salida,false);
+        //$contrato['alertDiff'] = $fecha_notificacion->diffInDays($fecha_salida,false);
+        if($contrato['alertDiff'] < 30){
+            if($contrato['dayDiff'] < 30){
+                $contrato['contractState'] = "Renovado";
+            }elseif($contrato['dayDiff'] < 45){
+                $contrato['contractState'] = "Notificación de renovación o terminación";
+            }
+        }
     }
-    echo "{\"prueba\":".$contratos->where('dayDiff','>',30)->where('dayDiff','<=',45)."}";
+    echo "{\"prueba\":".$contratos->whereBetween('dayDiff', [0, 45])."}";
 })->purpose('Display a list of users');
